cbeafd9788778ac15d29d9d7a81f8b153f9575b9
pepe <kato.aise@gmail.com>
2022-08-16 23:46:46 +0900

    UserGeneral,UserShop,Userクラスの内容変更とDAOの削除

--- U22/app/Library/User/UserGeneral.php
+++ U22/app/Library/User/UserGeneral.php
@@ -1,91 +1,30 @@
 <?php
 namespace App\Library\User;
-class UserGeneral{
-    private ?int $id = null;
-    private ?string $name = ""; //本名
-    private ?string $password = "";
-    private ?string $displayName = ""; //表示名
-    private ?int $loginWay = null; //ログイン方法
-    // private ?int $usage = null; //アカウント使用されているか
-    private ?string $loginValidityPeriod = ""; //ログイン有効期間
-    private ?int $loginToken = null; //ログイントークン
+use App\Library\User\User;
+
+class UserGeneral extends User
+{
+    private string $display_name;
 
     /**
-     * @return int ログイン方法？
+     * @param array{
+     *      display_name: string,
+     * }
      */
-    public function LoginWay(): int{
-        if($this->loginWay = 1){
-            //電話番号
-        }elseif($this->loginWay = 2){
-            //メールアドレス
-        }
-    }
-
-    /**
-     * @return bool アカウント使用されているか？
-     */
-    public function Usage(): bool{
-        // if(){
-
-        // }
+    public function __construct(array $user_info)
+    {
+        parent::__construct($user_info);
+        $this->display_name = $user_info["display_name"];
     }
 
 
     // 以下アクセサメソッド。
-    public function getId(): ?int{
-        return $this->id;
+    public function get_display_name(): ?string
+    {
+        return $this->display_name;
     }
-    public function setId(int $id): void{
-        $this->id = $id;
+    public function set_display_name(string $display_name): void
+    {
+        $this->display_name = $display_name;
     }
-    
-    public function getName(): ?string{
-        return $this->name;
-    }
-    public function setName(string $name): void{
-        $this->name = $name;
-    }
-
-    public function getPassword(): ?string{
-        return $this->password;
-    }
-    public function setPassword(string $password): void{
-        $this->password = $password;
-    }
-
-    public function getDisplayName(): ?string{
-        return $this->displayName;
-    }
-    public function setDisplayName(string $displayName): void{
-        $this->displayName = $displayName;
-    }
-
-    public function getLoginWay(): ?int{
-        return $this->loginWay;
-    }
-    public function setLoginWay(int $loginWay): void{
-        $this->loginWay = $loginWay;
-    }
-
-    public function getUsage(): ?int{
-        return $this->usage;
-    }
-    public function setUsage(int $usage): void{
-        $this->usage = $usage;
-    }
-
-    public function getLoginValidityPeriod(): ?string{
-        return $this->loginValidityPeriod;
-    }
-    public function setLoginValidityPeriod(string $loginValidityPeriod): void{
-        $this->loginValidityPeriod = $loginValidityPeriod;
-    }
-
-    public function getLoginToken(): ?int{
-        return $this->loginToken;
-    }
-    public function setLoginToken(int $loginToken): void{
-        $this->loginToken = $loginToken;
-    }
-
 }
--- U22/app/Library/User/UserShop.php
+++ U22/app/Library/User/UserShop.php
@@ -1,98 +1,42 @@
 <?php
 namespace App\Library\User;
-class UserShop{
-    private ?int $id = null;
-    private ?int $shopId = null;
-    private ?string $name = "";
-    private ?string $address = "";
-    private ?string $password = "";
-    private ?int $loginWay = null; //ログイン方法(１は電話番号、２はメアドなどconstで定義)
-    private ?string $loginValidityPeriod = ""; //ログイン有効期間(月日)
-    private ?string $loginToken = null; //ログイントークン(ランダムな文字列)
+use App\Library\User\User;
+
+class UserShop
+{
+    private int $shop_id;
+    private string $address;
 
     /**
-     * @return int ログイン方法？
+     * @param array{
+     *      shop_id: int,
+     *      address: string
+     * }
      */
-    public function LoginWay(): int{
-        if($this->loginWay = 1){
-            //電話番号
-        }elseif($this->loginWay = 2){
-            //メールアドレス
-        }
-    }
-
-    /**
-     * @return bool アカウント使用されているか？
-     */
-    public function Usage(): bool{
-        // if(){
-
-        // }
+    public function __construct(array $user_info)
+    {
+        parent::__construct($user_info);
+        $this->shop_id = $user_info["shop_id"];
+        $this->address = $user_info["address"];
     }
 
 
     // 以下アクセサメソッド。
-    public function getId(): ?int{
-        return $this->id;
+    public function get_shop_id(): ?int
+    {
+        return $this->shop_id;
     }
-    public function setId(int $id): void{
-        $this->id = $id;
+    public function set_shop_id(int $shop_id): void
+    {
+        $this->shop_id = $shop_id;
     }
-    
-    public function getShopId(): ?int{
-        return $this->shopId;
-    }
-    public function setShopId(int $shopId): void{
-        $this->shopId = $shopId;
-    }
-
-    public function getName(): ?string{
-        return $this->name;
-    }
-    public function setName(string $name): void{
-        $this->name = $name;
-    }
-
-    public function getAddress(): ?string{
+    public function get_address(): ?string
+    {
         return $this->address;
     }
-    public function setAddress(string $address): void{
+    public function set_address(string $address): void
+    {
         $this->address = $address;
-    }
-
-    public function getPassword(): ?string{
-        return $this->password;
-    }
-    public function setPassword(string $password): void{
-        $this->password = $password;
-    }
-
-    public function getLoginWay(): ?int{
-        return $this->loginWay;
-    }
-    public function setLoginWay(int $loginWay): void{
-        $this->loginWay = $loginWay;
-    }
-
-    public function getUsage(): ?int{
-        return $this->usage;
-    }
-    public function setUsage(int $usage): void{
-        $this->usage = $usage;
-    }
-
-    public function getLoginValidityPeriod(): ?string{
-        return $this->loginValidityPeriod;
-    }
-    public function setLoginValidityPeriod(string $loginValidityPeriod): void{
-        $this->loginValidityPeriod = $loginValidityPeriod;
-    }
-
-    public function getLoginToken(): ?int{
-        return $this->loginToken;
-    }
-    public function setLoginToken(int $loginToken): void{
-        $this->loginToken = $loginToken;
     }
 
 }
