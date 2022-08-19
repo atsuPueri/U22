972a7f09cabe6e7cb63c50fc27e263673ed8d6f8
shukanagai <88833966+shukanagai@users.noreply.github.com>
2022-08-19 22:27:05 +0900

    managerフォルダ追加

---
+++ U22/resources/views/manager/managerMenu.blade.php
@@ -0,0 +1,14 @@
+<nav id="managerMenu">
+    <div class="menuBtn">
+        <a href=""><img src="../img/parts/{{$menu['chat']['img']}}" alt=""></a>
+        <p class="{{$menu['chat']['name']}}">チャット</p>
+    </div>
+    <div class="menuBtn">
+        <a href=""><img src="../img/parts/{{$menu['lostList']['img']}}" alt=""></a>
+        <p class="{{$menu['lostList']['name']}}">忘れ物一覧</p>
+    </div>
+    <div class="menuBtn">
+        <a href=""><img src="../img/parts/{{$menu['resume']['img']}}" alt=""></a>
+        <p class="{{$menu['resume']['name']}}">プロフィール</p>  
+    </div>
+</nav>
