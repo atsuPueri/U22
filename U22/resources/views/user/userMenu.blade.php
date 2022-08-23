b3880903ca8cbbfb871750ce414d5a038d74f807
shukanagai <88833966+shukanagai@users.noreply.github.com>
2022-08-24 00:15:36 +0900

    アンカーにパス追加しました

--- U22/resources/views/user/userMenu.blade.php
+++ U22/resources/views/user/userMenu.blade.php
@@ -1,14 +1,14 @@
 <nav id="userMenu">
     <div class="menuBtn">
-        <a href=""><img src="./img/parts/{{$menu['chat']['img']}}" alt=""></a>
+        <a href="./userChatList"><img src="../img/parts/{{$menu['chat']['img']}}" alt=""></a>
         <p class="{{$menu['chat']['name']}}">チャット</p>
     </div>
     <div class="menuBtn">
-        <a href=""><img src="./img/parts/{{$menu['search']['img']}}" alt=""></a>
+        <a href="./search"><img src="../img/parts/{{$menu['search']['img']}}" alt=""></a>
         <p class="{{$menu['search']['name']}}">検索</p>
     </div>
     <div class="menuBtn">
-        <a href=""><img src="./img/parts/{{$menu['resume']['img']}}" alt=""></a>
+        <a href=""><img src="../img/parts/{{$menu['resume']['img']}}" alt=""></a>
         <p class="{{$menu['resume']['name']}}">プロフィール</p>
         
     </div>
