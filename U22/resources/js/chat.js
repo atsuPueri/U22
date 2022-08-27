3284472c151faa12e35f233ab8e5563cfa87d939
shukanagai <88833966+shukanagai@users.noreply.github.com>
2022-08-27 23:47:38 +0900

    チャットを一番下に表示させるjs

---
+++ U22/resources/js/chat.js
@@ -0,0 +1,8 @@
+// window.addEventListener('DOMContentLoaded', function() {
+    let chatArea = document.getElementById('chat');
+    // chatAreaHeight = chatArea.scrollHeight;
+    // chatArea.scrollTop = chatAreaHeight;
+//   })
+// chatArea.style.marginBottom = '80px';
+chatArea.scrollIntoView(false);
+
