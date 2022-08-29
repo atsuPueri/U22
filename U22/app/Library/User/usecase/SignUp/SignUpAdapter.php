3da652222819bf42c418ecf1f6bc91aa898f23de
pepe <kato.aise@gmail.com>
2022-08-29 12:46:41 +0900

    ちょっとだけ追追加

---
+++ U22/app/Library/User/usecase/SignUp/SignUpAdapter.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Library\User\usecase\SignUp;
+
+use App\Library\User\usecase\SignUp\SignUpPort;
+use App\Library\User\User\User;
+use Illminate\Support\Facades\DB;
+
+class SignUpAdapter implements SignUpPort
+{
+    /**
+     * ユーザー情報削除。
+     */
+    public function delete(int $id): bool
+    {
+        /**
+         * @param int is_shop 店舗側かユーザー側か
+         */
+        $is_shop = DB::table('user')->where('id', $id)->get('is_shop');
+
+        if($is_shop == 0){
+            $sql_delete = DB::table('user_general')
+            ->where('id', $id)
+            ->delete();
+            $sql_user_delete = DB::table('user')
+            ->where('id', $id)
+            ->delete();
+
+        }elseif($is_shop == 1){
+            $sql_delete = DB::table('user_shop')
+            ->where('id', $id)
+            ->delete();
+            $sql_user_delete = DB::table('user')
+            ->where('id', $id)
+            ->delete();
+        }
+        return $sql_delete !== 0;
+    }
+
+}
