55b2a67da2eefb0db78478b4b5043d9c4ec7385d
Chihiro0303 <hamaguchi.chihiro1@gmail.com>
2022-08-30 08:32:33 +0900

    検索画面、css

--- U22/resources/views/user/search.blade.php
+++ U22/resources/views/user/search.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/destyle.css">
+    <link rel="stylesheet" href="./css/userMenu.css">
     <link rel="stylesheet" href="./css/search.css">
     <title>検索画面</title>
 </head>
@@ -14,7 +15,7 @@
         <form action="#" class="search_container">
             <input type="text" placeholder="店舗名を入力してください">
             <button type="submit">
-                <img src="../img/虫眼鏡のアイコン.png" alt="">
+                <img src="./img/parts/search.png" alt="">
             </button>
         </form>
     </header>
@@ -22,33 +23,18 @@
         <article id="history">
             <h2>過去に訪れた店舗</h2>
             <ul>
+                @foreach($data as $content)
                 <li>
-                    <img src="../img/torikizoku_icon.jpeg" alt="">
-                    <h3>{{$storeName}}</h3>
-                    <p>梅田DDハウス店</p>
+                    <img src="./img/manager/{{$content['img']}}" alt="アイコン">
+                    <h3>{{$content['name']}}</h3>
+                    <p>{{$content['subName']}}</p>
                 </li>
-                <li>
-                    <img src="../img/torikizoku_icon.jpeg" alt="">
-                    <h3>鳥貴族</h3>
-                    <p>中崎町店</p>
-                </li>
-                <li>
-                    <img src="../img/torikizoku_icon.jpeg" alt="">
-                    <h3>鳥貴族</h3>
-                    <p>阪急東通り2号店</p>
-                </li>
-                <li>
-                    <img src="../img/torikizoku_icon.jpeg" alt="">
-                    <h3>鳥貴族</h3>
-                    <p>阪急東通り3号店</p>
-                </li>
+                @endforeach
             </ul>
         </article>
     </main>
     <footer>
-    <?php
-        require_once("./views/userMenu.blade.php");
-    ?>
+    <?php require_once '../resources/views/user/userMenu.blade.php'; ?>
     </footer>
 </body>
 </html>
