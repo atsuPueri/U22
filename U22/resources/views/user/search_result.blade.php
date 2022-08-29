293029f5a9d158f5f2894d49e2178004224d2e31
Chihiro0303 <hamaguchi.chihiro1@gmail.com>
2022-08-30 08:33:03 +0900

    検索結果画面

--- U22/resources/views/user/search_result.blade.php
+++ U22/resources/views/user/search_result.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/destyle.css">
+    <link rel="stylesheet" href="./css/userMenu.css">
     <link rel="stylesheet" href="./css/search_result.css">
     <title>検索結果</title>
 </head>
@@ -13,7 +14,7 @@
         <form action="#" class="search_container">
             <input type="text" placeholder="店舗名を入力してください">
             <button type="submit">
-                <img src="../img/虫眼鏡のアイコン.png" alt="">
+                <img src="./img/parts/search.png" alt="">
             </button>
         </form>
     </header>
@@ -21,99 +22,19 @@
         <article id="result">
             <h2 class="visuallyhidden">検索結果</h2>
             <ul>
+            @foreach($data as $content)
                 <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
+                    <img src="./img/manager/{{$content['img']}}" alt="アイコン">
+                    <h3>{{$content['name']}}</h3>
+                    <p>{{$content['address']}}</p>
+                    <p>{{$content['address2']}}</p>
                 </li>
-                <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="../public/img/torikizoku_icon.jpeg" alt="">
-                        <h3>鳥貴族 &nbsp; 梅田DDハウス店</h3>
-                        <p>大阪府 大阪市 北区芝田1-8-1 </p>
-                        <p>北野阪急ビル(D.D.HOUSE) 2階</p>
-                    </a>
-                </li>
+                @endforeach
             </ul>
         </article>
     </main>
     <footer>
-        <p>フッター</p>
+        <?php require_once '../resources/views/user/userMenu.blade.php'; ?>
     </footer>
 </body>
 </html>
