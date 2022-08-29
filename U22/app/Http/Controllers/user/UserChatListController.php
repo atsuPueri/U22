c35c015d75211383827ff0db0b216f6f9edb1e9f
atsu <yamahata.atsushi.as01@gmail.com>
2022-08-29 10:15:03 +0900

    途中までアップ

--- U22/app/Http/Controllers/user/UserChatListController.php
+++ U22/app/Http/Controllers/user/UserChatListController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Library\Chat\usecase\GetChatMessage\GetChatMessage;
 use App\Library\Chat\usecase\GetChatRoom\GetChatRoom;
 use App\Library\Chat\usecase\GetChatRoom\GetChatRoomAdapter;
+use Illuminate\Support\Collection;
 
 class UserChatListController extends Controller
 {
@@ -16,13 +17,39 @@ class UserChatListController extends Controller
         // $GetChatRoom = new GetChatRoom(new GetChatRoomAdapter);
 
         /** @var GetChatRoom */
-        // $GetChatRoom = \resolve(GetChatRoom::class);
+        $GetChatRoom = \resolve(GetChatRoom::class);
+
+        $room_array  = $GetChatRoom->get_all(1, GetChatRoom::USER_TYPE_GENERAL);
+
+        $map = collect($room_array)->map(function ($ChatRoom) {
+
+            $user_general =  $ChatRoom->to_UserGeneral();
+            return [
+                'managerId' => $user_general->get_id(),
+                'managerImgName' => 'kizoku.png',
+                'shopName' => '鳥貴族',//店名
+                'comment' => 'ご来店いただきありがとうございます。落とされたハンカチがどのようなものか特徴を教えていただけますか？',//最新のコメント表示
+                'commentNum' => '1',//コメント数
+            ];
+        });
 
         return view('user/userChatList' , [
-            'managerImgName' => 'kizoku.png',//店のプロフィール画像名
-            'shopName' => '鳥貴族',//店名
-            'comment' => 'ご来店いただきありがとうございます。落とされたハンカチがどのようなものか特徴を教えていただけますか？',//最新のコメント表示
-            'commentNum' => '1',//コメント数
+            "chatList" => [//チャット一覧の配列
+                [
+                    'managerId' => 0,//店のID(多分ページ遷移するときに使うかな？)
+                    'managerImgName' => 'kizoku.png',//店のプロフィール画像名
+                    'shopName' => '鳥貴族',//店名
+                    'comment' => 'ご来店いただきありがとうございます。落とされたハンカチがどのようなものか特徴を教えていただけますか？',//最新のコメント表示
+                    'commentNum' => '1',//コメント数
+                ],
+                [
+                    'managerId' => 1,//店のID(多分ページ遷移するときに使うかな？)
+                    'managerImgName' => 'kizoku.png',//店のプロフィール画像名
+                    'shopName' => '鳥貴族',//店名
+                    'comment' => 'ご来店いただきありがとうございます。落とされたハンカチがどのようなものか特徴を教えていただけますか？',//最新のコメント表示
+                    'commentNum' => '1',//コメント数
+                ]
+            ],
             'menu' => [
                 'chat' => ['img' => 'chatCheck.png', 'name' => 'check'],
                 'search' => ['img' => 'search.png', 'name' => 'notCheck'],
